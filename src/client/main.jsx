45c34030436b79b6106a31e8a34a68347d5e6774
Nick Golebiewski <nick.golebiewski@gmail.com>
2024-01-12 13:00:34 -0500

    added redux

--- src/client/main.jsx
+++ src/client/main.jsx
@@ -2,9 +2,11 @@ import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { store } from './store'
+import { Provider } from 'react-redux'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
- 
+  <Provider store={store}>
     <App />
-
+    </Provider>
 );
