e1b56310f5dff6b9bb8e533975c2361e7dbd2713
Nick Golebiewski <nick.golebiewski@gmail.com>
2024-01-23 16:15:48 -0500

    created login thunk and gear reducer for REDUX

--- src/client/Admin.jsx
+++ src/client/Admin.jsx
@@ -5,6 +5,7 @@ const Admin = () => {
   const token = useSelector(state => state.token.token)
   const user = useSelector(state => state.user.userRecord)
   const character = useSelector(state => state.userCharacter.character)
+  const gear = useSelector(state => state.gear.inventory)
 
   return (
     <section
@@ -14,8 +15,9 @@ const Admin = () => {
     <h1>check to see if admin for access</h1>
     <h1>Stuff in Redux State</h1>
     {token ? <p>TOKEN: {token}</p> : null}
-    {user? <p>USER: {user.username}...and more in the object! SUCCESS!</p> : null}
-    {character? <p>Character: {character.name}...and more in the object! SUCCESS!</p> : null}
+    {user? <p>USER: {user.username}</p> : null}
+    {character? <p>Character: {character.name} the {character.character_class}</p> : null}
+    {gear.length > 0? <div>Inventory: <ol>{gear.map((item) => <li>{item.name}</li>) }</ol></div> : null}
     </section>
   )
 }
---
+++ src/client/redux/thunks/authThunks.js
@@ -0,0 +1,68 @@
+import axios from 'axios';
+import { removeUserCharacter, setUserCharacter } from '../../reducers/characterSelectionSlice';
+import { removeToken, setToken } from '../../reducers/registrationSlice';
+import { setUser, removeUser } from '../../reducers/loginSlice';
+import { addItemToInventory, removeAllGear } from '../../reducers/gearSlice';
+
+export const loginThunk = (username, password, navigate) => async (dispatch, getState) => {
+  try {
+    //wipe state slate upon login//
+    dispatch(removeAllGear())
+    dispatch(removeUser())
+    dispatch(removeUserCharacter())
+    dispatch(removeToken())
+    ///////////////////////////////
+
+
+    const { data: token } = await axios.post("/auth/login", {
+        username,
+        password,
+    });
+
+    localStorage.setItem("TOKEN", token);
+    const userId = token.id;
+
+    dispatch(setToken(token.token));
+    dispatch(setUser({ id: userId }));
+
+    // Get User Record from DB and set it in state
+    const { data: userRecord } = await axios.get(`/api/user/${userId}`);
+    dispatch(removeUser());  // Clears User data out of state if already there
+    dispatch(setUser(userRecord));
+    console.log("user record set up in redux state");
+
+    // If no character is associated with the user yet, take them to the character selection page
+    if (!userRecord.character_id) { 
+        console.log("Navigate to character selection page");
+        navigate('/character');
+    }
+
+    // Load up character information into state
+    const { data: characterRecord } = await axios.get(`/api/character/${userRecord.character_id}`);
+    dispatch(setUserCharacter(characterRecord));
+    console.log("character record set up in redux state");
+
+    // Load up inventory into state
+    const { data: userInventory } = await axios.get(`/api/inventory/${userRecord.character_id}`);
+    console.log("UserInventory : ", userInventory)
+    userInventory.forEach( async (gearItem) => {
+      const { data: gearDetails } = await axios.get(`/api/gear/${gearItem.gear_id}`)
+      dispatch(addItemToInventory(gearDetails))
+      console.log(gearDetails.name, "added to Inventory in State")
+    });
+
+    console.log("login and character load up a SUCCESS");
+    localStorage.setItem("TOKEN", token);
+
+    // You can add additional logic here based on the success of the login
+    // For example, navigate to the game page or display an alert
+    if (token) {
+      console.log("Officially thunked. Navigate to the game page");
+      navigate('/game')
+    } else {
+        window.alert("Login Error");
+    }
+} catch (error) {
+    console.error(error);
+}
+};
