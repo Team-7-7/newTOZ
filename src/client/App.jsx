e51aab52f3edd6787149140afddefb2e844ec3a7
Nick Golebiewski <nick.golebiewski@gmail.com>
2024-01-29 14:51:29 -0500

    token login in progress

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,4 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
+import { useEffect } from 'react';
+import axios from 'axios';
 import About from './About';
 import Admin from './Admin';
 import Footer from './Footer';
@@ -12,22 +14,38 @@ import Profile from './Profile';
 import Register from './Register';
 import CharacterSelection from './CharacterSelection';
 
+
 const App = () => {
 
+  // useEffect(() => {
+  //   console.log("in use effect")
+  //   console.log("use effect-->token get", localStorage.getItem("TOKEN"))
+  //   try {
+  //     const token = localStorage.getItem("TOKEN")
+  //     const getUserFromToken = async () => {
+  //       const { data } = await axios.get("/api/user/me", {
+  //         headers: {
+  //           Authorization: "Bearer " + token,
+  //         },
+  //       })
+  //       console.log(data)
+  //       return data.userInfo.id
+  //     }
+  //     if (token) {
+  //       const id = getUserFromToken();
+  //       console.log(id)
 
+  //     }
+  //   } catch (error) { console.error(error) }
+  // }, []);
 
   return (
     <>
-      {/* <section id="header"> <Header /> </section>
-
-      <section id="navigation"> <Navigation /> </section> */}
-
-      {/* <section id="main" > */}
       <section>
 
         <Routes>
-          <Route id="main-section" path='/' element={<Home />}  />
-          <Route path='/about' element={<About/>}  />
+          <Route id="main-section" path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
           <Route path='/admin' element={<Admin />} />
           <Route path='/character' element={<CharacterSelection />} />
           <Route path='/game' element={<Game />} />
