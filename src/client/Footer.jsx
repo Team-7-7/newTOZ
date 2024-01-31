8105ad939dbe14603d937377d95932ddf1fb5744
Nick Golebiewski <nick.golebiewski@gmail.com>
2024-01-30 22:58:54 -0500

    updated github link on footer to new repo

--- src/client/Footer.jsx
+++ src/client/Footer.jsx
@@ -14,7 +14,7 @@ const Footer = () => {
   return (
     <div className="footer">
       <a id="fancy">Tower of Zurpalen </a>
-      <a className="nav" href="https://github.com/Team-7-7/tower_of_zurpalen" target="_blank">Source code on GitHub</a>
+      <a className="nav" href="https://github.com/Team-7-7/newTOZ/" target="_blank">Source code on GitHub</a>
       <Link className="nav" to="../about">About</Link>
       <Link className="nav" to="/login">Login</Link>
       <Link className="nav" to="/register">Register</Link>
